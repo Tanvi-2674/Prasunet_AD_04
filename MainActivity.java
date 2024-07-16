465aa57798f3e69d7c49993f4db846d299f8fc50
Tanvi <tanvikanjia@gmail.com>
2024-07-16 17:54:19 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button[][] buttons = new Button[3][3];
+    private boolean playerXTurn = true;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Play sound when the application is launching
+        mediaPlayer = MediaPlayer.create(this, R.raw.launching_sound);
+        mediaPlayer.start();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonId = "button_" + i + j;
+                int resId = getResources().getIdentifier(buttonId, "id", getPackageName());
+                buttons[i][j] = findViewById(resId);
+                buttons[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Button button = (Button) v;
+                        if (button.getText().toString().isEmpty()) {
+                            button.setText(playerXTurn ? "X" : "O");
+                            if (checkWin()) {
+                                Toast.makeText(MainActivity.this, "Player " + (playerXTurn ? "X" : "O") + " wins!", Toast.LENGTH_SHORT).show();
+                                // Play sound when a winner is declared
+                                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.win_sound);
+                                mediaPlayer.start();
+                                disableButtons();
+                            } else {
+                                playerXTurn = !playerXTurn;
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+        Button resetButton = findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetGame();
+            }
+        });
+    }
+
+    private boolean checkWin() {
+        for (int i = 0; i < 3; i++) {
+            if (buttons[i][0].getText().toString().equals(buttons[i][1].getText().toString()) &&
+                    buttons[i][1].getText().toString().equals(buttons[i][2].getText().toString()) &&
+                    !buttons[i][0].getText().toString().isEmpty()) {
+                return true;
+            }
+            if (buttons[0][i].getText().toString().equals(buttons[1][i].getText().toString()) &&
+                    buttons[1][i].getText().toString().equals(buttons[2][i].getText().toString()) &&
+                    !buttons[0][i].getText().toString().isEmpty()) {
+                return true;
+            }
+        }
+        if (buttons[0][0].getText().toString().equals(buttons[1][1].getText().toString()) &&
+                buttons[1][1].getText().toString().equals(buttons[2][2].getText().toString()) &&
+                !buttons[0][0].getText().toString().isEmpty()) {
+            return true;
+        }
+        if (buttons[0][2].getText().toString().equals(buttons[1][1].getText().toString()) &&
+                buttons[1][1].getText().toString().equals(buttons[2][0].getText().toString()) &&
+                !buttons[0][2].getText().toString().isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    private void disableButtons() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setEnabled(false);
+            }
+        }
+    }
+
+    private void resetGame() {
+        playerXTurn = true;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+                buttons[i][j].setEnabled(true);
+            }
+        }
+    }
+}
